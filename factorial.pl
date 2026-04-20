01a84667efeb78400e5a59b96e48962dbae8a2d2
RAVEENA <148243757+Raveena-29@users.noreply.github.com>
2026-04-20 21:29:14 +0530

    Create factorial.pl

---
+++ factorial.pl
@@ -0,0 +1,16 @@
+% Write a program in PROLOG to implement factorial (N, F) where F represents the factorial of a number N.
+
+% Base case: factorial of 0 is 1
+factorial(0, 1).
+
+% Recursive case
+factorial(N, F) :-
+    N > 0,
+    N1 is N - 1,
+    factorial(N1, F1),
+    F is N * F1.
+
+/*QUERIES TO RUN
+ * factorial(5, F).
+ * factorial(0, F).
+ */
